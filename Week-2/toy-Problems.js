481e4910325b073fb5295c4fdcf463fa8c60f51e
Jarrett Kennedy <kennedyjt88@gmail.com>
2016-06-20 15:14:15 -0700

    Create toy-Problems.js

---
+++ Week-2/toy-Problems.js
@@ -0,0 +1,20 @@
+// Sum of Positives
+// write a function that uses reduce, that returns the sum of only positive numbers in an array.
+/* CODE HERE */
+
+// each
+/* CODE HERE */
+
+// reduce
+/* CODE HERE */
+
+// filter with reduce
+/* CODE HERE */
+
+// map with reduce
+/* CODE HERE */
+
+// some with reduce
+// return true if some of the numbers in an array passes a test
+/* CODE HERE */
+
