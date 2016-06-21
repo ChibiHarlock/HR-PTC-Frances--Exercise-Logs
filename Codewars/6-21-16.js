a045a43f9d08438e7987504cbbf63c9bf47651ce
Jarrett Kennedy <kennedyjt88@gmail.com>
2016-06-21 14:47:46 -0700

    Update 6-21-16.js

--- Codewars/6-21-16.js
+++ Codewars/6-21-16.js
@@ -2,4 +2,59 @@
 Write a function that flattens an Array of Array objects into a flat Array. 
 Your function must only do one level of flattening.
 
+var array = {[1,2,3],[4,5,6],[7,8,9]}
+flatten(array); => [1,2,3,4,5,6,7,8,9]
+
 */
+// each
+function each (collection, callback){
+  
+  if (Array.isArray(collection)){
+    
+    for (var i = 0; i < collection.length; i++){
+      
+      callback (collection[i], i, collection);
+      
+    }
+    
+  } else {
+    
+    callback (collection[key], key, collection);
+    
+  }
+  
+}
+
+function reduce (array, callback, accumulator){
+  
+  each (array, function (value){
+    
+    if (accumulator === undefined){
+      
+      accumulator = value;
+      
+    } else {
+      
+      accumulator = callback (accumulator, value);
+      
+    }
+    
+    return accumulator;
+    
+  });
+  
+}
+
+function flatten (array){
+  
+  return reduce (array, function (result, curr){
+    
+    result.push(curr);
+    return result;
+    
+  }, []);
+  
+  
+}
+var array = {[1,2,3],[4,5,6],[7,8,9]}
+console.log(flatten(array));
