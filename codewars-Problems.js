c350db00f776e5bbc59b786fb0e2b9140475b93e
Jarrett Kennedy <kennedyjt88@gmail.com>
2016-06-21 09:16:47 -0700

    Create codewars-Problems.js

---
+++ codewars-Problems.js
@@ -0,0 +1,57 @@
+/*
+Make a function that receives a value, val and outputs the smallest higher number than the given value, 
+and this number belong to a set of positive integers that have the following properties:
+*/
+
+function each (collection, callback){
+
+  if (Array.isArray(collection)){
+  
+    for (var i = 0; i < collection.length; i++) {
+    
+      callback(collection[i], i, collection);
+    
+    }
+  
+  } else {
+  
+    for (var key in collection) {
+    
+      callback(collection[key], key, collection);
+    
+    }
+  
+  }
+
+}
+
+function reduce (array, callback, accumulator) {
+
+  each (array, function (value) {
+  
+    if (accumulator === undefined) {
+    
+      accumulator = value;
+    
+    } else {
+    
+      accumulator = callback (accumulator, value);
+    
+    }
+  
+  });
+  
+  return accumulator;
+
+}
+
+function nextGivenHigherNum (array) {
+
+  return reduce (array, function(smallNum, curr){
+  
+    if (curr > smallNum
+    return smallNum;
+  
+  }, 0);
+
+}
