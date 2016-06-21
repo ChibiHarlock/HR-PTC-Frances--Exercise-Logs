6521621f336cda89f924f924a74107f5d3697221
Jarrett Kennedy <kennedyjt88@gmail.com>
2016-06-21 12:27:31 -0700

    Update toy-Problems.js

--- Week-2/toy-Problems.js
+++ Week-2/toy-Problems.js
@@ -1,75 +1,137 @@
-/*
-Make a function that receives a value, val and outputs the smallest higher number than the given value, 
-and this number belong to a set of positive integers that have the following properties:
-*/
-
-function each (collection, callback){
-
-  if (Array.isArray(collection)){
+// each
+  /* CODE HERE */
+ +function each (collection, callback){
+ +  
+ +  if (Array.isArray(collection)){
+ +    
+ +    for (var i = 0; i < collection.length; i++){
+ +      
+ +      callback (collection[i], i, collection);
+ +      
+ +    }
+ +    
+ +  } else {
+ +    
+ +    for (var key in collection){
+ +      
+ +      callback (collection[key], key, collection);
+ +      
+ +    }
+ +    
+ +  }
+ +  
+ +}
   
-    for (var i = 0; i < collection.length; i++) {
-    
-      callback(collection[i], i, collection);
-    
-    }
+  // reduce
+  /* CODE HERE */
+ +function reduce (array, callback, accumulator){
+ +  
+ +  each (array, function (value){
+ +    
+ +    if (accumulator === undefined){
+ +      
+ +      accumulator = val;
+ +      
+ +    } else {
+ +      
+ +      accumulator = callback (accumulator, val);
+ +      
+ +    }
+ +    
+ +  });
+ +  
+ +  return accumulator;
+ +  
+ +}
+ +
+ +
+ +// Sum of Positives
+ +// write a function that uses reduce, that returns the sum of only positive numbers in an array.
+ +/* CODE HERE */
+ +// var num = [1, -2, 3, 4, -5, -6] => 8
+ +function sumOfPositives (array){
+ +  
+ +  return reduce (array, function (sum, curr){
+ +    
+ +    if (curr > 0){
+ +      
+ +      sum += curr;
+ +      
+ +    }
+ +    
+ +    return sum;
+ +    
+ +  }, 0);
+ +  
+ +}
   
-  } else {
+  // filter with reduce
+  /* CODE HERE */
+ +// var num = [1,2,3]; => [1,3]
+ +function filter (array, test){
+ +  
+ +  return reduce (array, function (result, curr){
+ +    
+ +    if (test(curr)){
+ +      
+ +      result.push(curr);
+ +      
+ +    }
+ +    
+ +    return result;
+ +    
+ +  }, []);
+ +  
+ +}
   
-    for (var key in collection) {
-    
-      callback(collection[key], key, collection);
-    
-    }
+  // map with reduce
+  /* CODE HERE */
+ +// var num = [1,2,4,5]
+ +function map (array, callback){
+ +  
+ +  return reduce (array, function (result, curr){
+ +    
+ +    result.push(callback(curr));
+ +    return result;
+ +    
+ +  }, []);
+ +  
+ +}
   
-  }
-
-}
-
-function reduce (array, callback, accumulator) {
-
-  each (array, function (value) {
+  // some with reduce
+  // return true if some of the numbers in an array passes a test
+  /* CODE HERE */
+ +function some (array, test){
+ +  
+ +  return reduce (array, function (hasValue, curr){
+ +    
+ +    if (test (curr)){
+ +      
+ +      hasValue = true;
+ +      
+ +    }
+ +    return hasValue;
+ +    
+ +  }, false);
+ +  
+ +}
   
-    if (accumulator === undefined) {
-    
-      accumulator = value;
-    
-    } else {
-    
-      accumulator = callback (accumulator, value);
-    
-    }
-  
-  });
-  
-  return accumulator;
-
-}
-
-function nextNumber (num, array) {
-
-  return reduce (array, function(smallNum, curr){
-  	
-  	// return the smallest number
-  	if (curr > num){
-  		if (curr % 2 !== 0){
-  			
-  			if ( curr % 3 === 0){
-  				
-  				smallNum = curr;
-  				
-  			}
-  			
-  		}
-  		
-  	} 
-  	
-  	
-  	
-    return smallNum;
-  
-  });
-
-}
-
-console.log(nextNumber(21, [28,3,9,4,25,30,35,15,105,5,2,12,14,21,18,19] )); // =. 105
-
+ +// minValue with reduce
+ +// return the mininum value of an array of numbers
+ +// var num = [4, 2, 1024, 2048, 9] => 2
+ +/* CODE HERE */
+ +function minValue (array){
+ +  
+ +  return reduce (array, function (min, curr){
+ +    
+ +    if (curr < min){
+ +      
+ +      min = curr;
+ +      
+ +    }
+ +    
+ +    return min;
+ +    
+ +  });
+ +  
+ +}
