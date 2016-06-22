6ece266d100a909cafb543fa0d53b07dd05aa453
Jarrett Kennedy <kennedyjt88@gmail.com>
2016-06-22 14:30:08 -0700

    Update toyProbelms2.js

--- Week-2/toyProbelms2.js
+++ Week-2/toyProbelms2.js
@@ -1,16 +1,45 @@
 // Largest items in a list
 // Write a function that returns the two largest items in a list(array)
 // sort the list if it's unsorted, you have no idea, so treat it like that
+// sort from small to big
 
 // eg. array = [1,89,23,5,43,900,88,243]
 // largest(3,array) => [89,243,900]
 
 // WRITE CODE HERE
-
+function largest(n , arr){
+  
+  // is the array unsorted?
+  arr.sort(function(a,b){
+    
+    return a - b;
+    
+  });
+  
+  // arr is now sorted [1,5,23,43,88,89,243,900]
+  
+  // slice method to remove n of the largest values
+  return arr.slice(-n);
+  
+  
+}
 
 // Without function
-// without([1,2,3,1,2,4], [1,2]) => [3,4]
 // returns items that are not in the exclusions array
 // exclusions will always be an array, with at least 1 value
 
+
+// without([1,2,3,1,2,4], [1,2]) => [3,4]
+
+
 // WRITE CODE HERE
+function without(arr, exclusions){
+
+  var value = exclusions;
+  return arr.reject(function(item){
+    
+    return value.indexOf(item) !== -1;
+    
+  });
+  
+}
