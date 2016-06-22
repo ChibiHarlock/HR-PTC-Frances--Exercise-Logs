aa003467fe1fb86fdc289c92c4957dd7e1e28121
Jarrett Kennedy <kennedyjt88@gmail.com>
2016-06-22 14:17:56 -0700

    Create toyProbelms2.js

---
+++ Week-2/toyProbelms2.js
@@ -0,0 +1,16 @@
+// Largest items in a list
+// Write a function that returns the two largest items in a list(array)
+// sort the list if it's unsorted, you have no idea, so treat it like that
+
+// eg. array = [1,89,23,5,43,900,88,243]
+// largest(3,array) => [89,243,900]
+
+// WRITE CODE HERE
+
+
+// Without function
+// without([1,2,3,1,2,4], [1,2]) => [3,4]
+// returns items that are not in the exclusions array
+// exclusions will always be an array, with at least 1 value
+
+// WRITE CODE HERE
