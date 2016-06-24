ce7d6490252bd4cdbac28b22468d58da2c89a7c8
Jarrett Kennedy <kennedyjt88@gmail.com>
2016-06-24 16:02:51 -0700

    added each, map(with each), and reduce(with each)

--- Underscore-JS/underscore.js
+++ Underscore-JS/underscore.js
@@ -23,8 +23,49 @@ function each (collection, iterator){
     }
   
   }
-  
-  
-
-
 }
+  
+// map with each
+/*
+map([1, 2, 3], function(num){ return num * 3; });
+=> [3, 6, 9]
+*/
+function map(arr,iterator){
+  
+  var result = [];
+  
+  each(arr, function(value){
+    
+    result.push(iterator(value));
+    
+  });
+  
+  return result;
+}
+
+// reduce with each
+/*
+var sum = reduce([1, 2, 3], function(memo, num){ return memo + num; }, 0);
+=> 6
+*/
+function reduce(arr, iterator, accumulator){
+  
+  each(arr, function(value){
+    
+    if(accumulator === undefined){
+      
+      accumulator = value;
+      
+    } else {
+      
+      accumulator = iterator(accumulator, value);
+      
+    }
+    
+  });
+  
+  return accumulator;
+  
+}
+
+
