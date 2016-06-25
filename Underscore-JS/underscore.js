c26e7ef6a0234da9afe32e5c9bb18e8c5f7f4f57
Jarrett Kennedy <kennedyjt88@gmail.com>
2016-06-25 08:17:19 -0700

    added reduceRight

--- Underscore-JS/underscore.js
+++ Underscore-JS/underscore.js
@@ -68,4 +68,37 @@ function reduce(arr, iterator, accumulator){
   
 }
 
+// reduceRight with each
+/*
+var list = [[0, 1], [2, 3], [4, 5]];
+var flat = reduceRight(list, function(a, b) { return a.concat(b); }, []);
+=> [4, 5, 2, 3, 0, 1]
+*/
 
+// in reduceRight, accum is 
+function reduceRight(arr, iterator, accumulator){
+  each(arr, function(value){
+    
+    if(accumulator === undefined){
+      
+      accumulator = value;
+      
+    } else {
+      // for reduceRight, the current value will be the first argument for the iterator
+      // e.g. reduce([1,2,3], function(a,b){return a+b;}); => 6     accum is undefined, so 1 is accum         iterator(1, 2)
+      // 1 + 2 = 3, iterator(3,3)  3+3 = 6
+      
+      // e.g. rightReduce([1],[2],[3]], function(a,b){return a.concat(b);},[]);  => [3,2,1]      accum is []
+      // [] = iterator([1],[])
+      // [1] = iterator([2],[1])
+      // [2,1] = iterator([3],[2,1])
+      // [3,2,1]
+      accumulator = iterator(value, accumulator);
+      
+    }
+    
+  });
+  
+  return accumulator;
+  
+}
