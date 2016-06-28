9c977ed0bb2610924b11c02465eb0d3f567dc322
Jarrett Kennedy <kennedyjt88@gmail.com>
2016-06-28 12:01:17 -0700

    added basic HR functions

--- Week3/practice.js
+++ Week3/practice.js
@@ -1,1 +1,70 @@
+// each
+function each(collection, callback){
+  
+  if(Array.isArray(collection)){
+    
+    collection.foreach(function(item, index, self){
+      
+      callback(item, index, self);
+      
+    });
+  } else {
+    
+    for(var key in collection){
+      
+      callback(collection[key], key, collection);
+      
+    }
+    
+  }
+  
+}
+
+// reduce
+function reduce(arr, callback, accumulator){
+  
+  each(arr, function(item){
+    
+    if(accumulator === undefined){
+      
+      accumulator = item;
+      
+    } else {
+      
+      accumulator = callback(accumulator, item);
+      
+    }
+    
+  });
+  
+  return accumulator;
+}
+
+// filter
+function filter(arr, test){
+  
+  return reduce(arr, function(passed, curr){
+    
+    if(test(curr)){
+      
+      passed.push(curr);
+      
+    }
+    return passed;
+    
+  }, []);
+  
+}
+
+// map
+function map(arr, callback){
+  
+  return reduce(arr, function(result, curr){
+    
+    result.push(curr);
+    return result;
+    
+  }, []);
+  
+}
 
