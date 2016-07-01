0e88c4b53526abc549c7ec559b2680156721b065
Jarrett Kennedy <kennedyjt88@gmail.com>
2016-07-01 11:20:20 -0700

    inital commit

---
+++ youCantJavaScriptUnderPressure/youCantJSUnderPressure.js
@@ -0,0 +1,37 @@
+// 1. create a function that returns the number doubled
+function doubleMe(n){
+
+  // code goes here
+
+}
+
+// 2. create a function that returns true if the number is even, and false otherwise
+function isEven(n){
+
+  // code goes here
+
+}
+
+// 3. create a function that returns only the extension filename of a string, or false
+function fileExtension(str){
+
+  // code goes here
+
+}
+
+
+// 4. create a function that returns the longest string in a array of strings.
+// the array can have other types like boolean and arrays, so check for that too
+function longestString(arr){
+
+  // code goes here
+
+}
+
+
+// 5. create a function that returns the sum of integers of an array, it should handle all nested levels
+function arraySum(arr){
+
+  // code goes here
+
+}
