95af3515f1dac5173133647cb3c86634d863afee
Jarrett Kennedy <kennedyjt88@gmail.com>
2016-07-01 16:55:12 -0700

    Create mock_practice3.js

---
+++ Week3/mock_practice3.js
@@ -0,0 +1,74 @@
+//1. Create an array with (Object that has the year), (string of your name), and (the number 5)
+
+
+//2. write the function timesTwo that multiplys the number by 2
+
+
+// 3. 
+// use timesTwo function on the number value in the object array
+
+
+// 4. Create each function that can work on objects, arrays and strings
+
+function each(collection, callback){
+  //your code here
+  
+}
+
+
+// 5.  Create reject function using each
+
+
+function reject(collection, callback){
+  //your code here
+  
+}
+
+
+// 6.  Create reduce function using each
+function reduce(collection, accumulator, startValue) {
+ 
+ //your code here
+}
+
+// 7. MAP
+
+function map(collection, callback) {
+  //your code here
+  
+}
+
+
+
+// 8.  Debug filter that uses reduce inside
+
+function = filter(collection predicate) 
+  var reduce(collection, predicate(item) {
+    if(!item)){
+      result.put(collection);
+    }
+
+    result = result + 1;
+  }, "");
+}
+
+
+// 9.  Create numberOfWins function using reduce
+//example
+numbersOfWins('LWLWLWWWLLWW')  // => {L: 5, W: 7};
+
+
+
+//10. Create default function using reduce and each
+
+}
+
+var iceCream = {flavor: "chocolate", temperature: "cold"};
+defaults(iceCream, {flavor: "vanilla", sprinkles: "lots"});
+=> {flavor: "chocolate", sprinkles: "lots", temperature: "cold"}
+
+
+
+
+
+
