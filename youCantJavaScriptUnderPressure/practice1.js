e9551126ee3333109ec9ca7fc344ecb10ba4392a
Jarrett Kennedy <kennedyjt88@gmail.com>
2016-07-01 12:36:30 -0700

    added solutions

--- youCantJavaScriptUnderPressure/practice1.js
+++ youCantJavaScriptUnderPressure/practice1.js
@@ -2,6 +2,7 @@
 function doubleMe(n){
 
   // code goes here
+  return n * 2;
 
 }
 
@@ -9,6 +10,7 @@ function doubleMe(n){
 function isEven(n){
 
   // code goes here
+  return (n % 2 === 0) ? true : false;
 
 }
 
@@ -16,6 +18,32 @@ function isEven(n){
 function fileExtension(str){
 
   // code goes here
+  var result = "" || false;
+  function each(collection, callback){
+    
+    if(typeof collection === "string"){
+      
+      for(var i = 0; i < collection.length; i++){
+        
+        callback(collection[i], i, collection);
+        
+      }
+      
+    }
+    
+  }
+  
+  each(n, function(item, index){
+    
+    if(item === "."){
+      
+      result = n.slice(index+1);
+      
+    }
+    
+  });
+  
+  return result;
 
 }
 
@@ -25,6 +53,25 @@ function fileExtension(str){
 function longestString(arr){
 
   // code goes here
+  // return the longest string in an array
+  // sort the array first from large to small by length
+  // return the sorted array that only have strings
+  // slice the first item of the list, which is the largest string
+  arr.sort(function(a,b){
+    
+    return b.length - a.length;
+    
+  });
+
+  return arr.filter(function(item){
+    
+    if(typeof item === "string"){
+      
+      return item;
+      
+    }
+    
+  }).slice(0,1);
 
 }
 
@@ -33,5 +80,16 @@ function longestString(arr){
 function arraySum(arr){
 
   // code goes here
+  // similar to flatten, handle all levels of depth
+  // use reduce to flatten the array and add integers to the startValue 
+  // if the curr value is an array, use recursion to run the function on the array again
+  // if the value is a number, then add that to the total sum, else add 0
+  // 0 is for any other value that is not a number
+  return arr.reduce(function(sum, isNested){
+    
+    sum += (Array.isArray(isNested)) ? arraySum(isNested) : (typeof isNested === "number") ? isNested : 0;
+    return sum;
+    
+  },0);
 
 }
