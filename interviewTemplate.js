a62c0f4155906aa6cf0bda283af68de91b95b134
Jarrett Kennedy <kennedyjt88@gmail.com>
2016-07-20 15:53:13 -0700

    inital commit

---
+++ interviewTemplate.js
@@ -0,0 +1,55 @@
+//1. // create an array myArray with numbers 1,2,3,4,5 in order
+
+//2. // create a object myObjects with key:value pairs for your name, email and phone number
+
+
+//3. create a function print that console logs a value
+
+//4. use your new print function to log the second element in your array
+
+//5. use print to log your name from myObjects
+// two ways to do this
+
+//6. create each function that handles arrays and objects
+
+
+//7. use each to return every element in your array
+
+
+//8. there's another way to use each with objects, do this
+
+//9. fix this filter code 
+function = filter(myArray)
+
+  each(myArray, filter(item){
+  
+    if(!item)
+    
+      result.put(filter);
+    
+    
+  
+  return result;
+  
+  }};
+
+
+}
+
+//10. 
+
+/*
+use filter with lists to return only the elements whose first element is a string
+
+write any additional functions you might need
+*/
+
+  
+  var list1 = [4, 'two', ['d', 'e']]; // would not return
+  var list2 = ['zero', 8, 2]; // would return
+  var list3 = [{'person': 'you'}, 'two', 9]; // would not return
+  
+  var lists = [list1,list2,list3];
+  
+  // filter 
+  
