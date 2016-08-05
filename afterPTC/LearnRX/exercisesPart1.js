4c40e739ea8c1fb8936f8a9ff2dcdb75ee9b3a29
Chibiharlock <kennedyjt88@gmail.com>
2016-08-05 09:24:17 -0700

    Hr solutions

--- afterPTC/LearnRX/exercisesPart1.js
+++ afterPTC/LearnRX/exercisesPart1.js
@@ -1,3 +1,18 @@
 /*
 *  Authored by Jarrett Kennedy 2016
 */
+
+var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"];
+
+// 1. print function to print all the names in the array
+function print(name){
+	console.log(name);
+}
+
+// 2. function to use print as a callback that prints all names
+function forEach(collection, callback){
+	collection.forEach(function(element, index){
+		callback(element);
+	});
+}
+forEach(names, print);
